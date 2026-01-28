9bb16cf2a500aecd56715998acfd3e3369500dbf
Cyrus Cox <55308997+ccox76@users.noreply.github.com>
2026-01-28 11:35:04 -0500

    init
    initial push with theoretical structure

---
+++ src/client.java
@@ -0,0 +1,41 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class client implements parser{
+    public static void main(String[] args) throws Exception{
+        //PLACEHOLDER FOR PARSER ARG CHECK
+        int clientID;
+
+        //init scanner for reading message
+        Scanner keyboard = new Scanner(System.in);
+        
+        String msg;
+
+        //await input from user, kill when command entered
+        while (true) {
+            do { 
+              //TAKE MESSAGES HERE, CHECK FOR PROPER SYNTAX IN WHILE
+            } while ();
+            //open channel
+            SocketChannel channel = SocketChannel.open();
+
+            //PLACEHOLDER FOR PARSER TO GET SWITCH ADDR
+            channel.connect(new InetSocketAddress());
+
+            //message buffer
+            ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+
+            channel.write(buffer);
+
+            //AWAIT REPONSE
+
+            channel.close();
+        }
+    }
+}
---
+++ src/host.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class host implements parser{
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
