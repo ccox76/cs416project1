10c5a54b9b02efd5c62727864244a4fe45d2d3ff
Cyrus Cox <55308997+ccox76@users.noreply.github.com>
2026-01-28 12:32:20 -0500

    update main function for switches
    add channel listener and main loop

--- src/switches.java
+++ src/switches.java
@@ -7,7 +7,17 @@ import java.nio.channels.SocketChannel;
 
 public class switches implements parser{
     public static void main(String[] args) throws Exception{
-        
+        ServerSocketChannel listenChannel = ServerSocketChannel.open();
+
+        //NEED PORT FOR BINDING
+        listenChannel.bind(new InetSocketAddress());
+
+        //main while loop
+        while (true) {
+            SocketChannel serveChannel = listenChannel.accept();
+            
+            String hostMessage = getUserInput(serveChannel);
+        }
     }
 
     static void printCache() throws IOException{
