36435083b531745f5cd87b9fce50e1689d7d9821
Smokinghalo8 <ethanfbuckner@gmail.com>
2026-02-02 10:25:48 -0500

    Parser init Push
    -Forgot to fully push this to the repo
    +Parser now has function to take in String, and return array with 3 items

--- src/parser.java
+++ src/parser.java
@@ -1,6 +1,16 @@
+import java.util.Arrays; //Using to look at tempArray
+
 public interface parser {
+    //functions: getRouteAndSplitMessage -> Take IDSender, IDRecipient, Message divide by commas and return array
 
-    //functions: GetRouteFromMessage -> Take IDSender, IDRecipement, Message divide by commas and return touple
 
+    static String[] getRouteAndSplitMessage(String message){
+        String[] tempArray = message.split(",");
+        tempArray[0] = tempArray[0].trim();
+        tempArray[1] = tempArray[1].trim();
+        tempArray[2] = tempArray[2].trim();
 
+        //System.out.println(Arrays.toString(tempArray));
+        return tempArray;
+    }
 }
