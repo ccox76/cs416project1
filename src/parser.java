0aee323a7dc252e3268f04cb66ce46b9aacbe2d1
Cyrus Cox <55308997+ccox76@users.noreply.github.com>
2026-02-05 17:15:33 -0500

    Update parser.java

--- src/parser.java
+++ src/parser.java
@@ -1,9 +1,14 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Arrays; //Using to look at tempArray
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays; 
+import java.util.HashMap;
 
 public interface parser {
     //functions: getRouteAndSplitMessage -> Take IDSender, IDRecipient, Message divide by commas and return array
-
     static String[] getRouteFromMessage(String message){
         String[] tempArray = message.split(",");
         tempArray[0] = tempArray[0].trim();
@@ -13,16 +18,28 @@ public interface parser {
         //System.out.println(Arrays.toString(tempArray));
         return tempArray;
     }
-    
-    static String[] getAddressFromConfig(String id, File config) {
-        
-        
-        return [IPAddr, portNum];
-    }
 
-    static String[] getLinksFromConfig(String id, File config) {
-        
-        
-        return linksList;
+    static HashMap<String, String[]> parseConfig(String id) {
+        Path configPath = Paths.get("src/config.txt");
+        String fileString;
+        BufferedReader br = new BufferedReader(new FileReader(configPath));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String currLine = br.readLine();
+
+            while (currLine != null) {
+                sb.append(currLine);
+                sb.append(System.lineSeparator());
+                currLine = br.readLine();
+            }
+            fileString = sb.toString();
+        } finally {
+            br.close();
+        }
+        HashMap idHash = new HashMap<String, String[]>();
+
+
+            
+        return idHash;
     }
 }
