23b2c21edf820114d2aff221be43d79bb18ab939
Cyrus Cox <55308997+ccox76@users.noreply.github.com>
2026-02-04 19:37:27 -0500

    funtion outlines

--- src/parser.java
+++ src/parser.java
@@ -1,10 +1,11 @@
+import java.io.File;
 import java.util.Arrays; //Using to look at tempArray
+
 
 public interface parser {
     //functions: getRouteAndSplitMessage -> Take IDSender, IDRecipient, Message divide by commas and return array
 
-
-    static String[] getRouteAndSplitMessage(String message){
+    static String[] getRouteFromMessage(String message){
         String[] tempArray = message.split(",");
         tempArray[0] = tempArray[0].trim();
         tempArray[1] = tempArray[1].trim();
@@ -12,5 +13,17 @@ public interface parser {
 
         //System.out.println(Arrays.toString(tempArray));
         return tempArray;
+    }
+    
+    static String[] getAddressFromConfig(String id, File config) {
+        
+        
+        return [IPAddr, portNum];
+    }
+
+    static String[] getLinksFromConfig(String id, File config) {
+        
+        
+        return linksList;
     }
 }
