eceb3420f59db64bbd828d3ea65b50895ea82953
Cyrus Cox <55308997+ccox76@users.noreply.github.com>
2026-02-05 22:47:09 -0500

    Add constructor to netcode, implement switchtable and such in swithes

--- src/netcode.java
+++ src/netcode.java
@@ -2,11 +2,16 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 public class netcode {
     private DatagramSocket socket;
 
     public record Data(String message, String sender, int senderPort) {};
+
+    public netcode(int port) throws SocketException{
+        this.socket = new DatagramSocket(port);
+    }
 
     public void send(String message, String reciever, int recieverPort) throws IOException {
         byte[] buffer = message.getBytes();
@@ -19,11 +24,11 @@ public class netcode {
         byte[] buffer = new byte[1024];
         DatagramPacket pckt = new DatagramPacket(buffer, buffer.length);
         socket.receive(pckt);
-         String message = new String(pckt.getData(), 0, pckt.getLength());
-         String sender = pckt.getAddress().getHostAddress();
-         int senderPort = pckt.getPort();
+        String message = new String(pckt.getData(), 0, pckt.getLength());
+        String sender = pckt.getAddress().getHostAddress();
+        int senderPort = pckt.getPort();
 
-         return new Data(message, sender, senderPort);
+        return new Data(message, sender, senderPort);
     }
 
     public void close() {
--- src/switches.java
+++ src/switches.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class switches implements parser {
     private netcode netcode;
-    Map<String, String> switchTable;
-    Map<String, String> virtualPorts;
+    Map<String, String> switchTable;//key: virtual addr [ip+port]
+    Map<String, String[]> virtualPorts; //key: Link name value: List [ip][port]
 
     public switches() {
         this.virtualPorts = new HashMap<>();
@@ -15,29 +15,55 @@ public class switches implements parser {
 
     private void getData(netcode.Data data) {
         String message = data.message();
-        String sender = data.sender();
-        int senderPort = data.senderPort();
         String[] sMessage = parser.getRouteFromMessage(message);
+        String sender = sMessage[0];
         String reciever = sMessage[1];
+        String fMessage = sMessage[2];
 
-        sendData(message, reciever);
+        if (!switchTable.containsKey(sender)) {
+            switchTable.put(sender, (virtualPorts.get(sender)[0]+":"+virtualPorts.get(sender)[1]));
+        }
+
+        sendData(fMessage, reciever);
     }
 
     private void sendData(String message, String reciever) {
         try {
-            netcode.send(message, reciever, 0);
+            if (!virtualPorts.containsKey(reciever)) { //NOT NEIGHBOR
+                for (Map.Entry<String, String[]> k : virtualPorts.entrySet()) {
+                    netcode.send(message, reciever, Integer.parseInt(k.getValue()[1]));
+                }
+            } else { //IS NEIGHBOR
+                netcode.send(message, reciever, Integer.parseInt(virtualPorts.get(reciever)[1]));
+            }
         } catch (IOException e) {
             System.err.println("Error sending frame");
+        }
+    }
+
+    private void printCache() {
+        System.out.println("Cache:");
+        for (Map.Entry<String, String> element : switchTable.entrySet()) {
+            System.out.println(switchTable.get(element));
         }
     }
     
     private void start(String id) throws IOException {
         HashMap<String, String[]> idHash = parser.parseConfig(id);
 
-        System.out.println("Swtich "+id+" initialized @"+ Arrays.toString(idHash.get("IP")) + Arrays.toString(idHash.get("Port")));
+        System.out.println("Switch "+id+" initialized @"+ Arrays.toString(idHash.get("IP")) + Arrays.toString(idHash.get("Port")));
+
+        HashMap<String, String[]> tempHash;
+
+        for (String link : idHash.get("Links")) {
+            tempHash = parser.parseConfig(link);
+            String[] list = {tempHash.get("IP")[0], tempHash.get("Port")[0]};
+            virtualPorts.put(link, list);  
+        }
 
         while (true) {
             try {
+                this.netcode = new netcode(Integer.parseInt(idHash.get("Port")[0]));
                 netcode.Data data = netcode.receive();
                 getData(data);
             } catch (IOException e) {
